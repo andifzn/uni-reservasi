1a77184e565d786a93e3092ee2b36c371bc3476d
andifzn <andifauzan586@gmail.com>
2026-07-19 10:53:56 +0800

    done

--- kamar-page/script.js
+++ kamar-page/script.js
@@ -1,3 +1,73 @@
+const supabaseUrl = "https://wldmnkdjgyxpacbjlwfr.supabase.co";
+const supabaseKey = "sb_publishable_Utwa-4_IPf7uGBAVA4M0uQ_5dX5fYvb";
+
+const client = window.supabase.createClient(supabaseUrl, supabaseKey);
+
+async function getRooms() {
+    const { data, error } = await client
+        .from("rooms")
+        .select("*");
+
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    tampilkanRooms(data);
+}
+
+function tampilkanRooms(rooms) {
+    const listKamar = document.getElementById("list-kamar");
+
+    listKamar.innerHTML = "";
+
+    rooms.forEach(room => {
+        listKamar.innerHTML += `
+            <div class="kamar-produk">
+
+                <div class="kamar-produk-img">
+                    <img src="../assets/img/${room.image}" alt="${room.name}">
+                </div>
+
+                <div class="kamar-produk-deskripsi">
+
+                    <h2>${room.name}</h2>
+
+                    <p>${room.description}</p>
+
+                    <div class="kamar-produk-detail">
+
+                        <div class="kapasitas-tamu">
+                            <img src="../assets/icons/guest.png">
+                            <p>${room.guest} Tamu</p>
+                        </div>
+
+                        <div class="jumlah-kasur">
+                            <img src="../assets/icons/bed-icon.png">
+                            <p>${room.bed}</p>
+                        </div>
+
+                        <p>Status : ${room.status}</p>
+
+                        <h3>
+                            Rp ${room.price.toLocaleString("id-ID")} / malam
+                        </h3>
+
+                        <a href="">
+                            Lihat Detail
+                        </a>
+
+                    </div>
+
+                </div>
+
+            </div>
+        `;
+    });
+}
+
+getRooms();
+
 const navbarMenu = document.querySelector(".navbar-menu");
 const hamburgerMenu = document.getElementById("hamburger-menu");
 
---
+++ website-admin/js/supabase.js
@@ -0,0 +1,7 @@
+const supabaseUrl = "...";
+const supabaseKey = "...";
+
+const client = window.supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+);
